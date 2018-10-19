25942ab487cebb82f0a4a8dd1da529066ffbc023
StrangrDangr <32497687+StrangrDangr@users.noreply.github.com>
2018-10-19 19:03:17 -0400

    finally working

--- hackventure.js
+++ hackventure.js
@@ -1,46 +1,75 @@
 window.onload =function(){
-
-	/*var theSitch = {node:0,question:"You are walking through the woods. The full moon hangs heavy in the sky.<br><br>You see a house with a light in the window. You've never seen a house in this part of the woods.<br><br>What do you do?",choices:["Get closer","Wait","Oh Hell No"]};*/
+	
+	
+	
 	var storyBoard = [
-		{node:0,question:"You are walking through the woods. The full moon hangs heavy in the sky.<br><br>You see a house with a light in the window. You've never seen a house in this part of the woods.<br><br>What do you do?",choices:["Get closer","Wait","Oh Hell No"]},
-		{node:1,question:"It sure is getting chilly, you need to do something",choices:["Get closer","Wait","Oh Hell No"]},
-		{node:2,question:"Well alright then. You turn to go when see something moving in the trees...",choices:["Go toward the house","I'm not scared!... But I'm not going near that house"]},
-		{node:3,question:"You hear a howl! There's a wolf coming out of the trees. Quick you better seek shelter!",choices:["Better go to that house, huh?"]},
-		{node:4,question:"You are approaching the house. A single candle is flickering in the window. Is that... music?",choices:["Let's do this. (Go in)","That's gonna be a no from me dawg."]},
-		{node:5,question:"You are at the door",choices:[]}
+		{node:0,
+			question:"You are walking through the woods. The full moon hangs heavy in the sky.<br><br>You see a house with a light in the window. You've never seen a house in this part of the woods.<br><br>What do you do?",
+			choices:[
+				"Get closer",
+				"Wait",
+				"Oh Hell No",
+				]},
+		{node:1,
+			question:"It sure is getting chilly, you need to do something",
+			choices:[
+				"Get closer",
+				"Wait",
+				"Oh Hell No"
+				]},
+		{node:2,
+			question:"Well alright then. You turn to go when see something moving in the trees...",
+			choices:[
+				"Go toward the house",
+				"I'm not scared!... But I'm not going near that house"
+				]},
+		{node:3,
+			question:"You hear a howl! There's a wolf coming out of the trees. Quick you better seek shelter!",
+				choices:[
+					"Better go to that house,huh?"
+					]},
+		{node:4,
+			question:"You are approaching the house. A single candle is flickering in the window. Is that... music?",
+			choices:[
+				"Let's do this. (Go in)",
+				"That's gonna be a no from me dawg."
+				]},
+		{node:5,
+			question:"You are at the door",
+			choices:[]}
 		];
-	function choices(x){
-		document.getElementById("choices").innerHTML="";
-		
-		
-		
-		
-		for (let counter=0; counter<x.choices.length; counter++){
-			let c=x.choices[counter];
-			if (c!=null){
-				let id='option' + x.node + "-" + counter;
-				document.getElementById("choices").innerHTML+= '<li id="' + id + '" data-q="' + x.node + '" data-a="' + counter + '">' + c + '</li>';
-				
-				document.getElementById(id).addEventListener("click",function(){
-						forward(this.getAttribute("data-q"),this.getAttribute("data-a"));
-						console.log(this.getAttribute("data-q") + "+" + this.getAttribute("data-a"));
-				});
-			};
-		}
-		document.getElementsByTagName("li").addEventListener("click",function(){
-				forward(this.getAttribute("data-q"),this.getAttribute("data-a"));
-				console.log(this.getAttribute("data-q") + "+" + this.getAttribute("data-a"));
-				});
-				
-			};
+
+
+
+	
 		
 		
 		
 	
 	function printSitch(q){
 		document.getElementById("question").innerHTML = q.question;
-		choices(q);
+		var node = q.node;
+		document.getElementById("choices").innerHTML="";
+		
+		function choices(choice,index){
+			if (choice!=null){
+				let id='option' + node + "-" + index;
+				document.getElementById("choices").innerHTML+= '<li id="' + id + '" data-q="' + node + '" data-a="' + index + '">' + choice + '</li>';
+			};
+		};
+		
+		q.choices.forEach(choices);
+		
+		var links = document.querySelectorAll('li');
+		for (i = 0; i < links.length; ++i) {
+			links[i].addEventListener("click",function(){
+				forward(this.getAttribute("data-q"),this.getAttribute("data-a"));
+				console.log(this.getAttribute("data-q") + "+" + this.getAttribute("data-a"));
+			});
+		};
 	};
+	
+	
 	function forward(q,a){
 		switch(q){
 			case "0":
@@ -123,18 +152,7 @@ window.onload =function(){
 		
 	};
 	printSitch(storyBoard[0]);
-
-  
 	
-	/*document.getElementById("question").innerHTML = "You are walking in woods. You see a house. What do you do?";
-	var question = 1
-	if (question === 1) {
-		document.getElementById("choices").innerHTML = "Go closer" //needs other choice
-	} else if (question === 2) {
-		document.getElementById("choices").innerHTML = "Not ready yet!"
-	} else {
-		console.log("Something went wong!")
-	}*/
 
 	console.log(storyBoard[0])
 	//function for choice click
